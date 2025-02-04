05c0916b8b59d223d534450667db0da7fc61dba6
ChrissiWi <ChrissiWi@users.noreply.github.com>
2025-02-04 08:34:43 +0100

    Test with DynamicData

--- ProgramTest/UnitTest1.cs
+++ ProgramTest/UnitTest1.cs
@@ -5,7 +5,24 @@ namespace ProgramTest;
 public class UnitTest1
 {
     [TestMethod]
-    public void TestMethod1()
+    [DynamicData(nameof(UnitTestData), DynamicDataSourceType.Method)]
+    public void Test_SplitByEnergy(List<Creature> creatures, int expectedLower)
     {
+        int expectedUpper = creatures.Count() - expectedLower;
+
+        (List<Creature> resultLower, List<Creature> resultHigher) = creatures.SplitByEnergy(5);
+
+        Assert.AreEqual(expectedLower, resultLower.Count());
+        Assert.AreEqual(expectedUpper, resultHigher.Count());
+    }
+
+    public static List<object[]> UnitTestData()
+    {
+        return new List<object[]>{
+            new object[] {new List<Creature>{new Elf("Legolas", 12)}, 0},
+            new object[] {new List<Creature>{new Bumbelbee("Maja", 2),
+                                             new Ghost("Edgar", 0),
+                                             new SlimeGhost("Allen", -1)}, 3}
+        };
     }
 }
